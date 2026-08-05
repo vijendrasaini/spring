1521757c22c7228c3b4a6ffb049a817185974088
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-05 20:12:54 +0530

    replicated time out issue

--- spring-boot-30-day/src/main/java/com/vijendra/day2/HikariCPDemo.java
+++ spring-boot-30-day/src/main/java/com/vijendra/day2/HikariCPDemo.java
@@ -7,10 +7,9 @@ import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 
 public class HikariCPDemo {
-    static void main(String[] args) throws SQLException {
+    static void main(String[] args) {
         HikariConfig hikariConfig = new HikariConfig();
 
         hikariConfig.setJdbcUrl("jdbc:mysql://localhost:3306/spring_boot_30_day");
@@ -20,44 +19,26 @@ public class HikariCPDemo {
         hikariConfig.setConnectionTimeout(2000);
 
         DataSource dataSource = new HikariDataSource(hikariConfig);
+        /*
+        Even data souce should be created inside try block. it closes connect pool.
+        * */
+
         // get a connection using try with resource
         String query = "select * from employees";
-/*        try(*/
+        try(
                 Connection connection = dataSource.getConnection();
-                PreparedStatement ps = connection.prepareStatement(query);
-/*                ) {*/
+                Connection connection2 = dataSource.getConnection();
+                PreparedStatement ps = connection.prepareStatement(query)
+                ) {
             System.out.println("Connection is obtained using HikariCP based datasource implementation.");
 
-/*            try(*/
-                    ResultSet rs = ps.executeQuery();
-/*            ) {*/
+            try(ResultSet rs = ps.executeQuery()) {
                 while(rs.next()) {
                     System.out.println("---------------Employee Details------------");
                     System.out.println("ID : " + rs.getInt("id"));
                     System.out.println("Name : " + rs.getString("name"));
                     System.out.println("Department : " + rs.getString("department"));
                     System.out.println("Salary : " + rs.getBigDecimal("salary"));
-                }
-/*            }*/
-/*        } catch (Exception e) {
-            System.out.println("Couldn't get a connection. Error Message : " + e.getMessage());
-        }*/
-
-        // again asking for a connect before closing the above one.
-        String singleRowQuery = "select * from employees where id = 1";
-        try(
-                Connection connection2 = dataSource.getConnection();
-                PreparedStatement ps2 = connection2.prepareStatement(query)
-        ) {
-            System.out.println("Connection is obtained 2nd time using HikariCP based datasource implementation.");
-
-            try(ResultSet rs2 = ps2.executeQuery()) {
-                if(rs2.next()) {
-                    System.out.println("---------------Employee Details------------");
-                    System.out.println("ID : " + rs2.getInt("id"));
-                    System.out.println("Name : " + rs2.getString("name"));
-                    System.out.println("Department : " + rs2.getString("department"));
-                    System.out.println("Salary : " + rs2.getBigDecimal("salary"));
                 }
             }
         } catch (Exception e) {
