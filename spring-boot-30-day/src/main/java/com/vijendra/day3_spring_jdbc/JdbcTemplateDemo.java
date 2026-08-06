65d11122be53280bfaa203d7a997a757734bf25f
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-06 20:12:41 +0530

    created jdbc template

--- spring-boot-30-day/src/main/java/com/vijendra/day3_spring_jdbc/JdbcTemplateDemo.java
+++ spring-boot-30-day/src/main/java/com/vijendra/day3_spring_jdbc/JdbcTemplateDemo.java
@@ -1,14 +1,15 @@
-package com.vijendra.day2;
+package com.vijendra.day3_spring_jdbc;
 
 import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 
-public class HikariCPDemo {
+public class JdbcTemplateDemo {
     static void main(String[] args) {
         HikariConfig hikariConfig = new HikariConfig();
 
@@ -19,30 +20,11 @@ public class HikariCPDemo {
         hikariConfig.setConnectionTimeout(2000);
 
         DataSource dataSource = new HikariDataSource(hikariConfig);
-        /*
-        Even data souce should be created inside try block. it closes connect pool.
-        * */
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
 
-        // get a connection using try with resource
-        String query = "select * from employees";
-        try(
-                Connection connection = dataSource.getConnection();
-                Connection connection2 = dataSource.getConnection();
-                PreparedStatement ps = connection.prepareStatement(query)
-                ) {
-            System.out.println("Connection is obtained using HikariCP based datasource implementation.");
+        String query = "select gpa from employees";
+        Integer employeesCount = jdbcTemplate.queryForObject(query, Integer.class);
 
-            try(ResultSet rs = ps.executeQuery()) {
-                while(rs.next()) {
-                    System.out.println("---------------Employee Details------------");
-                    System.out.println("ID : " + rs.getInt("id"));
-                    System.out.println("Name : " + rs.getString("name"));
-                    System.out.println("Department : " + rs.getString("department"));
-                    System.out.println("Salary : " + rs.getBigDecimal("salary"));
-                }
-            }
-        } catch (Exception e) {
-            System.out.println("Couldn't get a connection. Error Message : " + e.getMessage());
-        }
+        System.out.println("Total Employees : " + employeesCount);
     }
 }
