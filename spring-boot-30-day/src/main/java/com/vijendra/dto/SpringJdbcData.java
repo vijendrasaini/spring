76899d6dcf07afe446cb0af1e695f0c66d30e5fe
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-08 16:33:29 +0530

    introduced abstration for transaction manager

--- spring-boot-30-day/src/main/java/com/vijendra/dto/SpringJdbcData.java
+++ spring-boot-30-day/src/main/java/com/vijendra/dto/SpringJdbcData.java
@@ -2,8 +2,9 @@ package com.vijendra.dto;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
 
 public class SpringJdbcData {
     public JdbcTemplate jdbcTemplate;
-    public DataSourceTransactionManager transactionManager;
+    public PlatformTransactionManager transactionManager;
 }
