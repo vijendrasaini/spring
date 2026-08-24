9cf59b5b5f5c15014de06e8a4621e7f1b2f67ed3
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-24 15:50:39 +0530

    added mvc model

---
+++ spring-boot-30-day/src/main/java/com/vijendra/controller/TestController.java
@@ -0,0 +1,27 @@
+package com.vijendra.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.vijendra.model.Employee;
+import com.vijendra.service.EmployeeService;
+
+@RestController
+public class TestController {
+    private final EmployeeService employeeService;
+    public TestController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/")
+    public String index() {
+        return "Day 10 web layer is up";
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeService.getAllEmployees();
+    }
+}
