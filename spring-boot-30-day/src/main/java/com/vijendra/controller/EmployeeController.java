bd70a0663d6c98ec3520a57b4a2a41ac336e4e11
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-26 12:33:20 +0530

    explored exception handling on controller level

--- spring-boot-30-day/src/main/java/com/vijendra/controller/EmployeeController.java
+++ spring-boot-30-day/src/main/java/com/vijendra/controller/EmployeeController.java
@@ -2,7 +2,11 @@ package com.vijendra.controller;
 
 import java.util.List;
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vijendra.model.Employee;
@@ -46,5 +51,12 @@ public class EmployeeController {
     @PatchMapping("/{id}")
     public boolean updatEmployee(@RequestBody Employee employee, @PathVariable int id) {
         return this.employeeService.updateEmployeeName(id, employee.getName());
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(EmptyResultDataAccessException.class)
+    public ResponseEntity<String> handleNotFound(EmptyResultDataAccessException exception) {
+        // return "Employee Not found";
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Employee Not Found");
     }
 }
