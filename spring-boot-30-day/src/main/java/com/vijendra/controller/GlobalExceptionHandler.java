9d2ccbe26739aea93f9b7bacc93c63c4dc4efdad
Vijendra Saini <vijendrasaini0101+github@gmail.com>
2026-08-26 12:55:07 +0530

    added dto for error response

--- spring-boot-30-day/src/main/java/com/vijendra/controller/GlobalExceptionHandler.java
+++ spring-boot-30-day/src/main/java/com/vijendra/controller/GlobalExceptionHandler.java
@@ -6,11 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import com.vijendra.dao.ErrorResponse;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(EmptyResultDataAccessException.class)
-    public ResponseEntity<String> handleNotFound(EmptyResultDataAccessException exception) {
-        return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Data not found");
+    public ResponseEntity<ErrorResponse> handleNotFound(EmptyResultDataAccessException exception) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND)
+        .body(new ErrorResponse(404, "Data Not Found!"));
     }
 }
---
+++ spring-boot-30-day/src/main/java/com/vijendra/dao/ErrorResponse.java
@@ -0,0 +1,20 @@
+package com.vijendra.dao;
+
+import org.springframework.http.HttpStatus;
+
+public class ErrorResponse {
+    private int status;
+    private String message;
+    public ErrorResponse(int status, String message) {
+        this.status = status;
+        this.message = message;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
